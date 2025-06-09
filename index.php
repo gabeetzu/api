f016ac2f40435159a89fc3d112f7c31a77273a9a
gabeetzu <161157562+gabeetzu@users.noreply.github.com>
2025-06-09 20:22:45 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+set_error_handler(function($errno, $errstr, $errfile, $errline) {
+    $log = "[" . date("Y-m-d H:i:s") . "] PHP ERROR: $errstr in $errfile on line $errline\n";
+    file_put_contents("/var/data/logs/errors.csv", $log, FILE_APPEND);
+});
+
 // Simple index file for Render deployment
 header('Content-Type: application/json');
 
